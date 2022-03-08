9e9187721bb707d35d2f2f3a40586c2e5a1b699e
Banuprakash <banuprakashc@yahoo.co.in>
2022-03-08 10:12:19 +0530

    day 2

--- graphQLExample/src/main/java/com/adobe/demo/resolvers/BookFieldQueryResolver.java
+++ graphQLExample/src/main/java/com/adobe/demo/resolvers/BookFieldQueryResolver.java
@@ -1,5 +1,9 @@
 package com.adobe.demo.resolvers;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,17 +12,34 @@ import com.adobe.demo.entity.Book;
 import com.adobe.demo.entity.Publisher;
 
 import graphql.kickstart.tools.GraphQLResolver;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
-public class BookFieldQueryResolver implements GraphQLResolver<Book>{
-	 @Autowired
-	 private PublisherDao publisherDao;
-	 
-	 public Publisher getPublisher(Book book) {
-		 return publisherDao.findById(book.getPublisherId()).get();
-	 }
-	 
-	 public int getVersion(Book book) {
-		 return 9;
-	 }
+@Slf4j
+public class BookFieldQueryResolver implements GraphQLResolver<Book> {
+	@Autowired
+	private PublisherDao publisherDao;
+//	 
+//	 public Publisher getPublisher(Book book) {
+//		 return publisherDao.findById(book.getPublisherId()).get();
+//	 }
+
+	private final ExecutorService service = Executors.newFixedThreadPool(2);
+
+	public CompletableFuture<Publisher> getPublisher(Book book) {
+		return CompletableFuture.supplyAsync(() -> {
+			try {
+				log.info("Publisher request started {}", book.getId());
+				Publisher publisher = publisherDao.findById(book.getPublisherId()).get();
+				log.info("Publihser request completed {}", book.getId());
+				return publisher;
+			} catch (Exception e) {
+				return null;
+			}
+		}, service);
+	}
+
+	public int getVersion(Book book) {
+		return 9;
+	}
 }
